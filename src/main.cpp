edf03de7357dd23b6b9f486606d5f1d0c5640ae9
Ahmed Bodiwala <ahmed_bodi@msn.com>
2017-08-12 15:17:50 +0100

    Undo nVersion Checks for IsSuperMajority

--- src/main.cpp
+++ src/main.cpp
@@ -3077,6 +3077,7 @@ bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationState& sta
     if (pcheckpoint && nHeight < pcheckpoint->nHeight)
         return state.DoS(0, error("%s : forked chain older than last checkpoint (height %d)", __func__, nHeight));
 
+	/*
     // Reject block.nVersion=1 blocks when 95% (75% on testnet) of the network has upgraded:
     if (block.nVersion.GetBaseVersion() < 2 &&
         CBlockIndex::IsSuperMajority(2, pindexPrev, Params().RejectBlockOutdatedMajority()))
@@ -3090,7 +3091,7 @@ bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationState& sta
     {
         return state.Invalid(error("%s : rejected nVersion=2 block", __func__),
                              REJECT_OBSOLETE, "bad-version");
-    }
+    }*/
 
     return true;
 }
